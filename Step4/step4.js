2f754d1e6c57be672883b445480d0dee28f4ba40
Rafael <rafaelmarianorocha@gmail.com>
2018-07-09 18:50:34 +0000

    Adds completed Step4.

---
+++ Step4/step4.js
@@ -0,0 +1,36 @@
+var https = require('https');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+function printHTML (html) {
+  console.log(html);
+}
+
+function getAndPrintHTML (options, callback) {
+  var body = "";
+  /* Add your code here */
+  https.get(options, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (chunck) {
+    body += chunck;
+
+  });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  response.on('end', function() {
+    callback(body);
+  });
+
+});
+
+}
+
+getAndPrintHTML(requestOptions, printHTML);
